38353fb6fa9404d543b0c03ab662a9592daaaa5a
Tyler Blair <newlimitstyler@MacBookAir.lan>
2025-07-28 12:56:45 -0400

    Fix page scroll position on navigation - Add ScrollToTop component to automatically scroll to top when changing pages - Fixes issue where neighborhood pages and other pages were loading from bottom - Uses useLocation hook to detect route changes and scrollTo(0, 0) for instant top positioning

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Switch, Route } from "wouter";
+import { useEffect } from "react";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -16,22 +17,35 @@ import RelocationGuide from "@/pages/relocation-guide";
 import IDXSearch from "@/pages/idx-search";
 import NotFound from "@/pages/not-found";
 
+function ScrollToTop() {
+  const [location] = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  
+  return null;
+}
+
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/properties" component={IDXSearch} />
-      <Route path="/property/:id" component={PropertyDetail} />
-      <Route path="/about" component={About} />
-      <Route path="/neighborhoods" component={Neighborhoods} />
-      <Route path="/neighborhoods/:slug" component={NeighborhoodDetail} />
-      <Route path="/blog" component={Blog} />
-      <Route path="/blog/:slug" component={BlogPost} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/relocation-guide" component={RelocationGuide} />
-      <Route path="/idx-search" component={IDXSearch} />
-      <Route component={NotFound} />
-    </Switch>
+    <>
+      <ScrollToTop />
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/properties" component={IDXSearch} />
+        <Route path="/property/:id" component={PropertyDetail} />
+        <Route path="/about" component={About} />
+        <Route path="/neighborhoods" component={Neighborhoods} />
+        <Route path="/neighborhoods/:slug" component={NeighborhoodDetail} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/blog/:slug" component={BlogPost} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/relocation-guide" component={RelocationGuide} />
+        <Route path="/idx-search" component={IDXSearch} />
+        <Route component={NotFound} />
+      </Switch>
+    </>
   );
 }
 
